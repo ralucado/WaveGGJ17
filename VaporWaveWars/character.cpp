a738f260efccbd29b046ba09bca38cc0b0716b86
marti.lloveras <marti.lloveras@est.fib.upc.edu>
2017-01-21 01:53:45 +0100

    Character

--- VaporWaveWars/character.cpp
+++ VaporWaveWars/character.cpp
@@ -1,5 +1,40 @@
 #include "character.hpp"
 
 Character::Character(){
-
+    sf::Texture texture;
+    ASSERT(texture.loadFromFile(spriteFile));
+    height = texture.getSize().y;
+    width = texture.getSize().x;
+    next = 0;
+    setTexture(texture);
 }
+
+void Character::update(float deltaTime){
+    timestamp += deltaTime;
+    if (timestamp >= frameTime){
+        timestamp = 0;
+        if (actualState == previousState){
+            if (actualState == PlayerState::idle){
+                indexX = (indexX+1)%numFrames;
+            }
+            else if (actualState == PlayerState::attacking){
+                if (indexX == width){
+                    indexX = 0;
+                    actualState = PlayerState::idle;
+                }
+            }
+        }
+        else {
+            indexX = 0;
+            if (actualState == PlayerState::idle)
+                actualState = PlayerState::attacking;
+
+            else if (actualState == PlayerState::attacking)
+                actualState = PlayerState::idle;
+        }
+
+        sf::IntRect rect = sf::IntRect(indexX*width, indexY*height, width, height);
+        setTextureRect(rect);
+    }
+}
+
