19e293b238029fc3da205dcd8713111bffd5de27
Ralusama19 <ralucris.v@gmail.com>
2017-01-22 00:30:01 +0100

    no resize

--- VaporWaveWars/game.cpp
+++ VaporWaveWars/game.cpp
@@ -20,7 +20,7 @@ Game* Game::i() {
 // Main game loop
 void Game::run() {
     sf::Clock c;
-    sf::RenderWindow window(sf::VideoMode(W_WIDTH, W_HEIGHT), "( ( ( Radio Waves ) ) )");
+    sf::RenderWindow window(sf::VideoMode(W_WIDTH, W_HEIGHT), "( ( ( Radio Waves ) ) )", sf::Style::Close);
     window.setKeyRepeatEnabled(false);
     float oldTime = c.getElapsedTime().asSeconds();
     while (isRunning) {
