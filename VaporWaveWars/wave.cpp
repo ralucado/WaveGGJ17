d815176d01687f9e04178c361fe914f512fdf7a9
Ralusama19 <ralucris.v@gmail.com>
2017-01-22 01:53:35 +0100

    ones al miig

--- VaporWaveWars/wave.cpp
+++ VaporWaveWars/wave.cpp
@@ -11,7 +11,7 @@ Wave::Wave(bool dir){
     direction = dir;
     ASSERT(texture.loadFromFile(spriteFile));
     setTexture(texture);
-    setOrigin(texture.getSize().x/2,0);
+    setOrigin(texture.getSize().x/2,texture.getSize().y/2);
 //    sf::IntRect rect = sf::IntRect(0, 0, texture.getSize().x, texture.getSize().y);
 //    setTextureRect(rect);
 }
